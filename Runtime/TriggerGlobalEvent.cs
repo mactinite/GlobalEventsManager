393374cd2a925119f5bd419df413fa18a2d8ec17
Hunter MacDonald <mactinite77@gmail.com>
2021-08-26 20:59:40 -0700

    Add the ability to pass a float value to the trigger global events component

--- Runtime/TriggerGlobalEvent.cs
+++ Runtime/TriggerGlobalEvent.cs
@@ -8,11 +8,24 @@ namespace mactinite.GlobalEventsManager
     public class TriggerGlobalEvent : MonoBehaviour
     {
         public GlobalEvent gameEvent;
+        public string optionalParameterName;
 
         public void TriggerEvent()
         {
             GenericDictionary headers = new GenericDictionary();
             headers.SetHeaderValue("Position", (Vector2)transform.position);
+            GlobalEventsManager.TriggerEvent(gameEvent.name, headers);
+        }
+
+        public void TriggerFloatEvent(float value)
+        {
+            GenericDictionary headers = new GenericDictionary();
+            headers.SetHeaderValue("Position", (Vector2)transform.position);
+            if(!string.IsNullOrEmpty(optionalParameterName)){
+                headers.SetHeaderValue(optionalParameterName, value);
+            } else {
+                headers.SetHeaderValue("Value", value);
+            }
             GlobalEventsManager.TriggerEvent(gameEvent.name, headers);
         }
     }
